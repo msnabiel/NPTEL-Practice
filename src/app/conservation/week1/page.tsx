198544051d3a4512b8559a6ce76ff754bc65cdd3
msnabiel <89602212+msnabiel@users.noreply.github.com>
2025-04-10 16:38:45 +0530

    Update page.tsx

--- src/app/conservation/week1/page.tsx
+++ src/app/conservation/week1/page.tsx
@@ -17,7 +17,7 @@ export default function CategoriesPage() {
   const week = "week1";
 
   return (
-    <ContentLayout title="Forests and Their Management">
+    <ContentLayout title="Conservation Economics">
       <Breadcrumb>
         <BreadcrumbList>
           <BreadcrumbItem>
