d4c92ae9a7b1d50e0387110a64ad3a3f87b969ec
msnabiel <89602212+msnabiel@users.noreply.github.com>
2025-04-16 22:22:19 +0530

    Create head.tsx

---
+++ src/app/head.tsx
@@ -0,0 +1,8 @@
+export default function Head() {
+  return (
+    <>
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+    </>
+  );
+}
