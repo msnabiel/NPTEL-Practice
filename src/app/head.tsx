316b31d2fe675e8751effce2057e58f0d7792f83
msnabiel <89602212+msnabiel@users.noreply.github.com>
2025-04-16 22:27:13 +0530

    Update head.tsx

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,8 +1,9 @@
 export default function Head() {
   return (
-    <>
+    <head>
+      <meta property="og:image" content="https://nptel-vit.vercel.app/og-image.png" />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
-    </>
+    </head>
   );
 }
