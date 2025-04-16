d096ed947099e6e4523e6af965ab88dc0b882fd3
msnabiel <89602212+msnabiel@users.noreply.github.com>
2025-04-16 19:31:06 +0530

    Update content-layout.tsx

--- src/components/admin-panel/content-layout.tsx
+++ src/components/admin-panel/content-layout.tsx
@@ -1,4 +1,5 @@
 import { Navbar } from "@/components/admin-panel/navbar";
+import { Analytics } from "@vercel/analytics/next"; // Import Analytics
 
 interface ContentLayoutProps {
   title: string;
@@ -8,6 +9,7 @@ interface ContentLayoutProps {
 export function ContentLayout({ title, children }: ContentLayoutProps) {
   return (
     <div>
+      <Analytics /> {/* Place the Analytics component here */}
       <Navbar title={title} />
       <div className="container pt-8 pb-8 px-4 sm:px-8">{children}</div>
     </div>
