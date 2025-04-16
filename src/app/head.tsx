5e420c4795529448f2430beca9cc3f90fde09e16
msnabiel <89602212+msnabiel@users.noreply.github.com>
2025-04-16 22:54:03 +0530

    Update head.tsx

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -4,6 +4,7 @@ export default function Head() {
       <meta property="og:image" content="https://nptel-vit.vercel.app/og-image.png" />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
+      <meta property="og:site_name" content="VIT NPTEL Quiz Hub" />
     </head>
   );
 }
