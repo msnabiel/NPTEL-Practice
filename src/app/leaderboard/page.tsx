56fd6437fe89bf390865b29cc0198de94ddecda1
msnabiel <89602212+msnabiel@users.noreply.github.com>
2025-04-17 18:50:02 +0530

    Update page.tsx

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -53,8 +53,8 @@ export default function LeaderboardPage() {
   const [selectedCourse, setSelectedCourse] = useState<string>("All Courses");
   const [isRefreshing, setIsRefreshing] = useState(false);
   const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+     process.env.SUPABASE_URL!,
+    process.env.SUPABASE_ANON_KEY!
   );
   // Fetch leaderboard data from Supabase
   const fetchData = async () => {
